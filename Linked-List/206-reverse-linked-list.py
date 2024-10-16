5edfd004c04379318a79f2e81cdbf3d9d89c4a97
HuyTu <dragon10x0408@gmail.com>
2024-10-16 14:48:37 +0700

    Linked list

--- Linked-List/206-reverse-linked-list.py
+++ Linked-List/206-reverse-linked-list.py
@@ -1,3 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBeginning(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head  # Next for new node becomes the   current head
+        self.head = new_node  # Head now points to the new node
+
+    def insertAtEnd(self, new_data):
+        new_node = Node(new_data)
+
+        # If the list is empty, make the new node the head
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+
+        # Otherwise, traverse the list to find the last node
+        while last.next:
+            last = last.next
+        last.next = new_node  # Make the new node the next node of the last node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=' ')
+            temp = temp.next
+        print()  # Ensures the output is followed by a new line
+
+
 """
 https://leetcode.com/problems/reverse-linked-list/
 
@@ -11,11 +48,6 @@ thì lúc duyệt 1 qua 2 thì mình trỏ lại
 và tiếp tục duyệt 2 qua 3
 1 <- 2 <- 3
 """
-# class ListNode(object):
-#      def __init__(self, val=0, next=None):
-#          self.val = val
-#          self.next = next
-
 
 class Solution(object):
     def reverseList(self, head):
@@ -25,7 +57,7 @@ class Solution(object):
         """
         if not head:
             return head
-        prev = ne = None
+        prev = None
 
         while head:
             ne = prev
@@ -36,3 +68,21 @@ class Solution(object):
 
 
 if __name__ == '__main__':
+    llist = LinkedList()
+    # [1, 2, 3, 4, 5]
+    llist.insertAtBeginning(5)
+    llist.insertAtBeginning(4)
+    llist.insertAtBeginning(3)
+    llist.insertAtBeginning(2)
+    llist.insertAtBeginning(1)
+
+    # [1, 2]
+    # llist.insertAtBeginning(2)
+    # llist.insertAtBeginning(1)
+
+    # []
+
+    llist.printList()
+
+    llist.head = Solution().reverseList(llist.head)
+    llist.printList()
