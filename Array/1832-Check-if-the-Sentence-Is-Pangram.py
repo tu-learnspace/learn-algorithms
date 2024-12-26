7a8ab5e41d33c6ab5bd6b172e96a42359630cccd
HuyTu <dragon10x0408@gmail.com>
2024-12-26 15:06:22 +0700

    Check if the Sentence Is Pangram

--- Array/1832-Check-if-the-Sentence-Is-Pangram.py
+++ Array/1832-Check-if-the-Sentence-Is-Pangram.py
@@ -12,6 +12,18 @@ class Solution(object):
         """
         return len(set(sentence)) == 26
 
+        """
+        Nếu không muốn add hết vào set
+        """
+        # seen = set()
+        #
+        # for i in range(len(sentence)):
+        #     currChar = sentence[i].lower()
+        #
+        #     if currChar.isalpha():
+        #         seen.add(currChar)
+        #
+        # return len(seen) == 26
 
 
 if __name__ == '__main__':
