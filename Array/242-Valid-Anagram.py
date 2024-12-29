41768b2610433cc9c458d3b919c4c5a4da7e218b
HuyTu <dragon10x0408@gmail.com>
2024-12-29 13:14:21 +0700

    Valid Anagram

---
+++ Array/242-Valid-Anagram.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/valid-anagram/
+
+Dùng frequency map để check, tập dụng luôn 1 map đó. Nếu 2 thằng là anagram thì map phải bằng 0 hết.
+-> 1 string mang đơn vị giá trị dương, string còn lại mang giá trị âm
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        freq = {}
+        for i in range (len(s)):
+            if s[i] in freq:
+                freq[s[i]] += 1
+            else:
+                freq[s[i]] = 1
+
+            if t[i] in freq:
+                freq[t[i]] -= 1
+            else:
+                freq[t[i]] = -1
+
+        for char in freq:
+            if freq[char] != 0:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    s = "anagram"
+    t = "nagaram"
+    res = Solution().isAnagram(s, t)
+    print(res)
