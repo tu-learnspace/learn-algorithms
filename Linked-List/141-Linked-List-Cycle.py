4997e8003a720afa9d00b5860fd87be6bc94a543
HuyTu <dragon10x0408@gmail.com>
2025-01-14 23:39:37 +0700

    fast slow pointer

--- Linked-List/141-Linked-List-Cycle.py
+++ Linked-List/141-Linked-List-Cycle.py
@@ -12,17 +12,20 @@ Lưu ý: Đề bài không ràng buộc giá trị không bị lặp lại -> gi
 
 
 Cách 2: Slow and Fast pointers
-Dùng 2 con trỏ, 1 con chạy nhanh, 1 con chạy chậm. Nếu 2 con trỏ gặp nhau thì return True
+Định luật học thuộc: nếu có 2 racers chạy trong 1 vòng tròn, 1 racer nhanh hơn racer còn lại -> racer nhanh hơn chắc chắn
+sẽ đuổi kịp & vượt qua racer chậm hơn từ phía sau. Nếu không có vòng thì racer nhanh hơn sẽ về đích (null) trước, còn có
+vòng thì racer chậm sẽ gặp đc racer nhanh.
+Lý giải dễ hiểu: vd cả 2 con trỏ đều vô vòng, liệu nó có gặp nhau được không?
+Khi con trỏ nhanh approach con trỏ chậm từ đằng sau:
+- Nếu con trỏ nhanh 1 bước chậm hơn: Thì step tiếp theo, con trỏ nhanh đi 2 bước, con trỏ chậm đi 1 bước -> gặp nhau.
+- Nếu con trỏ nhanh 2 bước chậm hơn: Thì step tiếp theo, con trỏ nhanh sẽ chậm hơn con trỏ chậm 1 bước (quay về scenario 1)
+
+-> Dùng 2 con trỏ, 1 con chạy nhanh, 1 con chạy chậm. Nếu 2 con trỏ gặp nhau thì return True
 - Time: O(N)
 - Space: O(1)
 
-Giải thích về cách 2:
-- Nếu có cycle thì con trỏ chạy nhanh sẽ gặp con trỏ chạy chậm
-- Nếu không có cycle thì con trỏ chạy nhanh sẽ gặp null trước con trỏ chạy chậm
-
-p1 đi được a bước thì p2 đi được 2a bước
-gọi x là số node của p1 đi đc trên vòng
-
+Giải thích về cách 2 (??):
+p1 đi được a bước thì p2 đi được 2a bước. gọi x là số node của p1 đi đc trên vòng
 rùa gặp thỏ: vị trí rùa = vị trí thỏ
 => a + x % b = a + (a + 2x) % b
 => x % b = (a + 2x) % b
@@ -31,7 +34,6 @@ rùa gặp thỏ: vị trí rùa = vị trí thỏ
 => x = -a + kb (Chọn k nhỏ nhất cho x có nghĩa)
 -> Rùa và thỏ gặp nhau tại tọa độ x
 -> nếu tại vị trí đó mà đi thêm a bước nữa, rùa sẽ vào vị trí node bắt đầu của cycle
-
 """
 # Definition for singly-linked list.
 class ListNode:
