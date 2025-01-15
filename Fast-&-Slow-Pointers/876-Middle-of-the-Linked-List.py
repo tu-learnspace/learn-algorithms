72dbeb21629b75024b9ec0cf3ae1cd7c5b5e84c6
HuyTu <dragon10x0408@gmail.com>
2025-01-15 14:09:20 +0700

    Middle of the Linked List

--- Fast-&-Slow-Pointers/876-Middle-of-the-Linked-List.py
+++ Fast-&-Slow-Pointers/876-Middle-of-the-Linked-List.py
@@ -4,6 +4,8 @@ https://leetcode.com/problems/middle-of-the-linked-list
 Tìm phẩn tử ở giữa LL, nếu có 2 node ở giữ thì return cái phía sau
 
 Idea ======
+Dùng 1 con trỏ chậm đi bth, con trỏ nhanh đi nhanh x2 -> khi con trỏ nhanh chạm cuối list thì con trỏ chậm ở giữa list.
+Note 1 điều là khi check đk ta phải check fast trước (vì fast ko có thì fast.next sẽ lỗi object has no attribute 'next')
 """
 # Definition for singly-linked list.
 class ListNode(object):
@@ -28,7 +30,7 @@ if __name__ == '__main__':
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
     head.next.next.next.next = ListNode(5)
-    # head.next.next.next.next.next = ListNode(6)
+    head.next.next.next.next.next = ListNode(6)
 
     res = Solution().middleNode(head)
     print(res.val)
