bbdb31462cd7398b09a07388d62322d9f10765f6
HuyTu <dragon10x0408@gmail.com>
2025-01-18 00:14:47 +0700

    Palindrome linked list

--- Fast-&-Slow-Pointers/234-Palindrome-Linked-List.py
+++ Fast-&-Slow-Pointers/234-Palindrome-Linked-List.py
@@ -13,18 +13,16 @@ VD:
 1. Dùng fast, slow. Khi này fast ở cuối, slow ở middle.
 1 -> 2 -> 3 -> 2 -> 1
           s
-2. Reverse nửa sau 3 -> 2 -> 1 thành 3 <- 2 <-1. Cứ move slow đi, dùng 1 biến temp để giữ lại vị trí previous để point về
-p = s
-s = s.next
-p    s
-3 -> 2 -> 1
-s.next = p
+2. Reverse nửa sau 3 -> 2 -> 1 -> null thành null <- 3 <- 2 <-1. Cứ move slow đi, dùng 1 biến temp để giữ lại vị trí next để point tiếp
+null <- 3 -> 2 -> 1 -> null
+prev    s   tmp
 
-p = s
-s = s.next
-     p    s
-3 <- 2 <- 1
-s.next = p
+tmp = s.next
+s.next = prev
+prev = s
+s = tmp
+
+3. Compare 2 nửa
 """
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -36,25 +34,27 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        # find middle.
+        if head is None or head.next is None:
+            return True
+
+        # 1. find middle.
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # reverse the second half.
-        prev = slow # prev to keep the old position of slow for it to point back
-        slow = slow.next
+        # 2. reverse the second half.
+        prev = None # prev to keep the old position of slow for it to point back
         while slow:
+            tmp = slow.next
             slow.next = prev
             prev = slow
-            slow = slow.next
+            slow = tmp
 
-        # check 2 part if they are equal.
-        slow = prev
-        while slow:
+        # 3. check 2 part if they are equal.
+        slow = prev # slow cuối cùng bị move vô null rồi nên phải lấy prev
+        while head and slow:
             if head.val != slow.val:
-                print('aa')
                 return False
             head = head.next
             slow = slow.next
