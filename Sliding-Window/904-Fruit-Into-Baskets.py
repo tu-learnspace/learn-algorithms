d5ebd7a5e216d05d76db7c208d5dcc40c7b8c929
HuyTu <dragon10x0408@gmail.com>
2025-02-03 16:21:43 +0700

    fruit into baskets

--- Sliding-Window/904-Fruit-Into-Baskets.py
+++ Sliding-Window/904-Fruit-Into-Baskets.py
@@ -1,10 +1,10 @@
 """
 https://leetcode.com/problems/fruit-into-baskets
 Đề ======
-Tính số quả nhiều nhất có thể thu hoạch. Input là array các tree. arr[i] là loại quả tại cây thứ i.
-Được cho 2 basket, mỗi basket chỉ đc 1 loại quả, k giới hạn số trái.
+Tính số quả nhiều nhất có thể thu hoạch. Input là array các tree. arr[i] là LOẠI quả tại cây thứ i.
+Được cho 2 basket, mỗi basket chỉ đc 1 LOẠI quả, k giới hạn số trái.
 Có thể bắt đầu từ bất kỳ cây nào. Tại mỗi cây chỉ đc pick 1 quả, move dần sang phải, không được skip cây.
-Cứ pick tới khi nào ko thể tiếp tục aka pick loại quả thứ 3.
+Cứ pick tới khi nào ko thể tiếp tục.
 
 Input: fruits = [1,2,1]
 Output: 3
@@ -19,6 +19,8 @@ Output: 4
 Explanation: We can pick from trees [2,3,2,2]. If we had started at the first tree, we would only pick from trees [1,2].
 
 Idea =====
+Pick tới khi ko thể tiếp tục -> aka dừng khi pick loại quả thứ 3.
+Dùng sliding window, tạo cửa sổ bằng cách expand bên phải, tới khi ko thỏa nữa thì shrink bên trái, cập nhật max, repeat.
 """
 class Solution(object):
     def totalFruit(self, fruits):
@@ -26,4 +28,30 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
+        fruits_frequency = {}
+        start, count = 0, 0
+        max_fruits = float('-inf')
+
+        for end in range(len(fruits)):
+            if fruits[end] not in fruits_frequency:
+                fruits_frequency[fruits[end]] = 0
+
+            fruits_frequency[fruits[end]] += 1
+            count += 1
+
+            while len(fruits_frequency) > 2:
+                fruits_frequency[fruits[start]] -= 1
+                count -= 1
+                if fruits_frequency[fruits[start]] == 0:
+                    del fruits_frequency[fruits[start]]
+
+                start += 1
+
+            max_fruits = max(max_fruits, count)
+
+        return max_fruits
+
 if __name__ == '__main__':
+    fruits = [3,3,3,1,2,1,1,2,3,3,4]
+    res = Solution().totalFruit(fruits)
+    print(res)
