1d616e493ce003f9acf83d5137b1c7d3b93d844e
HuyTu <dragon10x0408@gmail.com>
2025-02-08 23:53:10 +0700

    Max consecutive ones iii

--- Sliding-Window/1004-Max-Consecutive-Ones-III.py
+++ Sliding-Window/1004-Max-Consecutive-Ones-III.py
@@ -9,7 +9,10 @@ Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 
 Idea =====
-
+Sliding window y như longest repeating character replacement: maintain start vs end là window's boundaries. Expand window
+bằng cách move end sang phải, khi nào vượt quá k thì shrink bên trái để có đk at most k -> đảm bảo keep track longest
+subarray mà không cần check all possible subarray.
+Tuy nhiên, đỡ hơn là chỉ có 0 vs 1 nên k cần hash map để store frequency.
 """
 class Solution(object):
     def longestOnes(self, nums, k):
@@ -18,11 +21,6 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-
-        """ 
-          .       .
-        1,1,1,0,0,0,1,1,1,1,0
-        """
         start, max_len, max_repeated_one = 0, 0, 0
 
         for end in range(len(nums)):
@@ -30,13 +28,12 @@ class Solution(object):
                 max_repeated_one += 1
 
             while end - start + 1 > k + max_repeated_one:
-                start += 1
                 if nums[start] == 1:
                     max_repeated_one -= 1
-
-
+                start += 1
 
             max_len = max(max_len, end - start + 1)
+
         return max_len
 
 if __name__ == '__main__':
