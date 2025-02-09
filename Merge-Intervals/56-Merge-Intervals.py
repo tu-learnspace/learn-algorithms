794bf54bfe1b6763a8e560ef827f16142caaf325
HuyTu <dragon10x0408@gmail.com>
2025-02-09 22:21:53 +0700

    merge intervals

---
+++ Merge-Intervals/56-Merge-Intervals.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/merge-intervals/
+Đề =========
+
+
+Idea ==========
+
+"""
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+
+if __name__ == '__main__':
