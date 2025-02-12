7a5156996a87f6ecf31eec2c797857747286f0d7
HuyTu <dragon10x0408@gmail.com>
2025-02-12 10:45:41 +0700

    meeting rooms

--- Merge-Intervals/252-Meeting-Rooms.py
+++ Merge-Intervals/252-Meeting-Rooms.py
@@ -11,10 +11,20 @@ Appointments: [[6,7], [2,4], [13, 14], [8,12], [45, 47]]
 Output: true
 Explanation: None of the appointments overlap, therefore a person can attend all of them.
 
+Idea ====
+Sort theo start rồi so sánh [i+1].start < i.end là có conflict
 """
 class Solution(object):
     def canAttendAllAppointments(self, intervals):
-        return False
+        intervals.sort(key = lambda x: x[0])
 
+        for i in range(len(intervals) - 1):
+            if intervals[i + 1][0]  < intervals[i][1]:
+                return False
+        return True
 
 if __name__ == '__main__':
+    appointments = [[6, 7], [2, 4], [13, 14], [8, 12], [45, 47]]
+    # appointments = [[1,4], [2,5], [7,9]]
+    res = Solution().canAttendAllAppointments(appointments)
+    print(res)
