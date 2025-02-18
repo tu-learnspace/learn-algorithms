088cb8db0062128b5f81be9419af397a5a106718
HuyTu <dragon10x0408@gmail.com>
2025-02-19 00:04:26 +0700

    interval list intersections

--- Merge-Intervals/986-Interval-List-Intersections.py
+++ Merge-Intervals/986-Interval-List-Intersections.py
@@ -13,7 +13,24 @@ class Solution(object):
         :type secondList: List[List[int]]
         :rtype: List[List[int]]
         """
+        merged_intervals = []
 
+        i, j = 0, 0
+        while i < len(firstList) and j < len(secondList):
+            # 1st list overlap 2nd list or vice versa
+            if (secondList[j][0] <= firstList[i][0] <= secondList[j][1] or
+                    firstList[i][0] <=  secondList[j][0] <= firstList[i][1]):
+                merged_intervals.append([max(firstList[i][0], secondList[j][1]), min(firstList[i][1], secondList[j][1])])
+
+            # a:  --    ---
+            # b: -----
+            # Cái end trc (bị merge rồi) thì move next
+            if firstList[i][1] < secondList[j][1]:
+                i += 1
+            else:
+                j += 1
+
+        return merged_intervals
 
 if __name__ == '__main__':
     firstList = [[0, 2], [5, 10], [13, 23], [24, 25]]
