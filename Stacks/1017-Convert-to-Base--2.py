e563b787531ad4d36ae98b9430b59f0acd295b12
HuyTu <dragon10x0408@gmail.com>
2025-02-20 23:53:03 +0700

    convert to base -2

---
+++ Stacks/1017-Convert-to-Base--2.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/convert-to-base-2
+Đề ====
+Convert thành base -2
+
+Input: n = 2
+Output: "110"
+Explantion: (-2)^2 + (-2)^1 = 2
+
+
+Input: n = 3
+Output: "111"
+Explantion: (-2)^2 + (-2)^1 + (-2)^0 = 3
+
+Idea ====
+"""
+class Solution(object):
+    def baseNeg2(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+
+
+if __name__ == '__main__':
