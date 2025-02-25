55800e1cec172032d1a37e434993aa28faba09b3
HuyTu <dragon10x0408@gmail.com>
2025-02-26 00:19:47 +0700

    convert to base -2

--- Stacks/1017-Convert-to-Base--2.py
+++ Stacks/1017-Convert-to-Base--2.py
@@ -7,12 +7,17 @@ Input: n = 2
 Output: "110"
 Explantion: (-2)^2 + (-2)^1 = 2
 
-
 Input: n = 3
 Output: "111"
 Explantion: (-2)^2 + (-2)^1 + (-2)^0 = 3
 
 Idea ====
+Quy đổi decimal sang binary thì ta chia n cho 2 (tới khi n = 0) rồi lưu remainder (số dư) lại. Remainder khi chia 2 có
+thể là 1 hoặc 0:
+- n % 2 -> dùng n & 1 (extract last bit, 1 or 0)
+- n / 2 -> dùng n >>= 1
+Tuy nhiên, với số âm thì khi chia âm, remainder có thể bị âm (k đc vì remainder chỉ đc là 0 hoặc 1) -> enforce remainder
+là dương, thì số bị âm là số bị chia (n) -> thêm âm khi n/2: n = - (n << 1)
 """
 class Solution(object):
     def baseNeg2(self, n):
@@ -21,9 +26,12 @@ class Solution(object):
         :rtype: str
         """
         stack = []
-        while (n):
-
-            n >>= 1
-
+        while n:
+            stack.append(n & 1) # remainder
+            n = -(n >> 1) # divided by -2
+        return ''.join(str(i) for i in reversed(stack))
 
 if __name__ == '__main__':
+    n = 3
+    res = Solution().baseNeg2(n)
+    print(res)
