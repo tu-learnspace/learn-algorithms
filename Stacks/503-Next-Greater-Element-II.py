0f8af1b8622c8f4afa51095fc15b736fef4d1e9c
HuyTu <dragon10x0408@gmail.com>
2025-02-27 21:55:42 +0700

    next greater element ii

---
+++ Stacks/503-Next-Greater-Element-II.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/next-greater-element-ii
+Đề =========
+
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2;
+The number 2 can't find next greater number.
+The second 1's next greater number needs to search circularly, which is also 2.
+
+Input: nums = [1,2,3,4,3]
+Output: [2,3,4,-1,4]
+
+Idea ========
+"""
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+if __name__ == '__main__':
+
