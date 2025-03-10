4a635c38448f705c3ba887433386a128aba4ae47
HuyTu <dragon10x0408@gmail.com>
2025-03-10 23:50:17 +0700

    next greater element ii

--- Stacks/503-Next-Greater-Element-II.py
+++ Stacks/503-Next-Greater-Element-II.py
@@ -19,8 +19,24 @@ Output: [-1, 12, 12, -1]
 Idea ========
 Bắt đàu từ cuối vì phần tử cuối k có next greater nên cuối cùng luôn là -1.
 
-[13, 14, 7, 6, 12]
+[13,14,5,7,6,12]
+[-1,-1,-1,-1,-1]         {12]
 
+12 lớn hơn 6 nên đưa 12 vô res. Sau đó push 6 vô để check sau (biết đâu 6 là NGE của ai đó)
+[13,14,5,  7, 6, 12]
+[-1,-1,-1,-1,12, -1]   {6, 12]
+
+7 lớn hơn 6 nên phải pop những phần tử bé hơn 7 ra, cho vào res (which is 12), sau đó đưa 7 vào
+[13,14,5, 7, 6, 12]
+[-1,-1,-1,12,12,-1]   {7, 12]
+
+5 ko bé hơn 7 nên cho 7 vô res
+[13,14,5, 7, 6, 12]
+[-1,-1,7,12,12,-1]   {7, 12]
+
+Tương tự, 14 lớn hơn nên cho pop ra hết, empty stack
+[13,14,5, 7, 6, 12]
+[-1,-1,7,12,12,-1]   {14]
 """
 class Solution(object):
     def nextGreaterElements(self, nums):
@@ -42,6 +58,7 @@ class Solution(object):
         return res
 
 if __name__ == '__main__':
-    nums = [4, 5, 2, 25]
+    # nums = [4, 5, 2, 25]
+    nums = [13,14,5,7,6,12]
     res = Solution().nextGreaterElements(nums)
     print(res)
