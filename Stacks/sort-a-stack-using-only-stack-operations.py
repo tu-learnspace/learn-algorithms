4f462391972ecfbeb9462b739debead5a0093c77
HuyTu <dragon10x0408@gmail.com>
2025-03-11 23:49:36 +0700

    sort stack using only push and pop

---
+++ Stacks/sort-a-stack-using-only-stack-operations.py
@@ -0,0 +1,29 @@
+"""
+== Đề
+Given a stack, sort it using only stack operations (push and pop).
+
+You can use an additional temporary stack, but you may not copy the elements into any other data structure (such as an array).
+The values in the stack are to be sorted in descending order, with the largest elements on top.
+
+
+1. Input: [34, 3, 31, 98, 92, 23]
+   Output: [3, 23, 31, 34, 92, 98]
+
+2. Input: [4, 3, 2, 10, 12, 1, 5, 6]
+   Output: [1, 2, 3, 4, 5, 6, 10, 12]
+
+3. Input: [20, 10, -5, -1]
+   Output: [-5, -1, 10, 20]
+
+== Idea
+"""
+class Solution(object):
+    def sortStack(self,stack):
+        tempStack = []
+        # ToDo: Write Your Code Here.
+        return tempStack
+
+if __name__ == '__main__':
+    stack = [34, 3, 31, 98, 92, 23]
+    res = Solution().sortStack(stack)
+    print(res)
